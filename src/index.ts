d68d664d92e64809c11063bff0ebdb186068a17b
Gabriel Drouin <info@gabrieldrouin.com>
2024-12-27 15:51:45 -0500

    feat: add second server to fetch from first server

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ const port = process.env.PORT ?? "9001";
 
 app.get("/", middleware);
 
+app.get("/data", (req, res) => {
+  console.log("Request headers:", req.headers, `\n\n\n`);
+  res.status(200).json({ name: "Jason" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
