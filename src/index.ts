5a0de46d47ec5f4b569b1fe50a313414fe727b2e
Gabriel Drouin <info@gabrieldrouin.com>
2024-12-30 22:34:04 -0500

    feat: add cookies

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 //import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { middleware } from "#middlewares/middlewares.js";
+import cookieParser from "cookie-parser";
 import routes from "#routes/index.js";
 
 const app = express();
@@ -12,9 +12,17 @@ const port = process.env.PORT ?? "9001";
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("tiny"));
+app.use(cookieParser("supersecret"));
+
 app.use(routes);
 
-app.get("/", middleware);
+app.get("/", (req, res) => {
+  res.cookie("name", "val", { maxAge: 5000, signed: true });
+  res.cookie("name2", "val2", { maxAge: 5000, signed: true });
+  res.send({ msg: "set a cookie" });
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -2,8 +2,17 @@ import { Router } from "express";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {
-  res.send({ id: "1", name: "keyboard", quantity: 42 });
-});
+interface Cookies {
+  name?: string;
+  name2?: string;
+}
 
+productRouter.get("/", (req, res) => {
+  const cookies = req.signedCookies as Cookies;
+  if (cookies.name) console.log(cookies.name);
+  if (cookies.name2) console.log(cookies.name2);
+
+  const product = { id: "1", name: "keyboard", quantity: 42 };
+  res.send(product);
+});
 export default productRouter;
