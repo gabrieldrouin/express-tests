15fd45308bad9cf8ea4848e86a9b6dd5f3488384
Gabriel Drouin <info@gabrieldrouin.com>
2024-12-31 15:36:00 -0500

    feat: set basic session cookie

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import routes from "#routes/index.js";
 
 const app = express();
@@ -13,15 +14,32 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(cookieParser("supersecret"));
-
+app.use(
+  session({
+    secret: "supersecret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 10000,
+    },
+  }),
+);
 app.use(routes);
 
+declare module "express-session" {
+  interface SessionData {
+    visited: boolean;
+  }
+}
+
 app.get("/", (req, res) => {
-  res.cookie("name", "val", { maxAge: 5000, signed: true });
-  res.cookie("name2", "val2", { maxAge: 5000, signed: true });
-  res.send({ msg: "set a cookie" });
-  console.log(req.headers.cookie);
-  console.log(req.cookies);
+  console.log(req.sessionID);
+  req.session.visited = true;
+  //res.cookie("name", "val", { maxAge: 5000, signed: true });
+  //res.cookie("name2", "val2", { maxAge: 5000, signed: true });
+  res.send({ msg: "set a cookie." });
+  //console.log(req.headers.cookie);
+  //console.log(req.cookies);
 });
 
 app.listen(port, () => {
